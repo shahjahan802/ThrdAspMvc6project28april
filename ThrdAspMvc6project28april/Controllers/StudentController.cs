181a0cb436850abe9c6167c5f8927d34b628d0b3
shahjahan <shahjahan7868@outlook.com>
2018-04-28 12:19:36 -0700

    structure and oop approach use in  clind to server side data pass but some issue raise

---
+++ ThrdAspMvc6project28april/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ThrdAspMvc6project28april.Models;
+
+namespace ThrdAspMvc6project28april.Controllers
+{
+    public class StudentController : Controller
+    {    [HttpGet]  
+        public IActionResult StudentView()
+        {
+            return View();
+        } 
+        [HttpPost]
+        public IActionResult StudentView( Student obj)
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult StudentOld()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult StudentOld(string Name ,string Course)
+        {
+            return View();
+        }
+    }
+}
---
+++ ThrdAspMvc6project28april/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThrdAspMvc6project28april.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string course { get; set; }
+
+    }
+}
